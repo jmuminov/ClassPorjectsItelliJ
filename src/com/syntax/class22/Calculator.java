e4ced51e7570a01fd256efc8e73a5739047b3e8d
jmuminov <jmuminov92@gmail.com>
2021-09-01 19:03:05 -0400

    Class 22 commit

---
+++ src/com/syntax/class22/Calculator.java
@@ -0,0 +1,12 @@
+package com.syntax.class22;
+
+import java.sql.SQLOutput;
+
+public class Calculator {
+    void addInt(int num1, int num2) {
+        System.out.println(num1+num2);
+    }
+    void addDouble(double num1, double num2) {
+        System.out.println(num1+num2);
+    }
+}
---
+++ src/com/syntax/class22/CalculatorTester.java
@@ -0,0 +1,10 @@
+package com.syntax.class22;
+
+public class CalculatorTester {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addDouble(14.5, 12.5);
+
+    }
+}
